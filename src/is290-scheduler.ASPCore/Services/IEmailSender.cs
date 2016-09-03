92a3ce47a14c20d2a287b7c89692eba46747d609
Joe Arcelo <josepharcelo@gmail.com>
2016-09-03 15:51:00 -0500

    Add project files.

---
+++ src/is290-scheduler.ASPCore/Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace is290_scheduler.ASPCore.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
