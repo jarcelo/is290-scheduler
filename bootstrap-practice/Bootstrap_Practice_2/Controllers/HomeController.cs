c80c456fa43f7901059af7cb5b799a14242e3ce7
kmne68 <kmne68@gmail.com>
2016-10-25 20:25:45 -0500

    Add bootstrap practice folder

---
+++ bootstrap-practice/Bootstrap_Practice_2/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Bootstrap_Practice_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.emery = "hello";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult TimeTrack()
+        {
+            ViewBag.Message = "Track time here.";
+
+            return View();
+        }
+    }
+}
